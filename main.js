567f987a731db097a4c0f8dfe147c39d88983870
Asimsaeed111 <161206949+Asimsaeed111@users.noreply.github.com>
2024-03-05 14:17:19 +0500

    Day 2 Coding challange

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+var famousperson = 'Albert Einstein';
+console.log(`${famousperson} once said, "A person who never made a mistake never tried anything new."`);
+var famous_person = 'Albert';
+var massage = `${famous_person} once said, "A person who never made a mistake never tried anything new."`;
+console.log(massage);
+let myname = "\t\n Asim saeed \t\n";
+console.log(myname);
+console.log(myname.trim());
---
+++ main.ts
@@ -0,0 +1,8 @@
+var famousperson: string = 'Albert Einstein';
+console.log(`${famousperson} once said, "A person who never made a mistake never tried anything new."`);
+var famous_person: string = 'Albert';
+var massage = `${famous_person} once said, "A person who never made a mistake never tried anything new."`);
+console.log(massage);
+let myname: string = "\t\n Asim saeed \t\n";
+console.log(myname) 
+console.log(myname.trim());
